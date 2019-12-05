e74d3644430039fa45b51eec624f1985d1469d59
Jesse Tippit <jtippit223@gmail.com>
2019-12-05 04:41:32 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -100,7 +100,7 @@ def gross_per_studio(collection)
 
   total
 end
-end
+
 
 def movies_with_directors_set(source)
   # GOAL: For each director, find their :movies Array and stick it in a new Array
